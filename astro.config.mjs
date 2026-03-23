cb3ff17e80565ac9023973ad7e87c4bd011eee5f
Alexander Campos <notaprompt@users.noreply.github.com>
2026-03-22 21:35:52 -0400

    Update domain to campos.works

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://acampos.dev',
+  site: 'https://campos.works',
   prefetch: true,
   integrations: [sitemap()],
 });
